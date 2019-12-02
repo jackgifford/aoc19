3b9f773a2f5a0d258f5f8d3cbf4e3e2a337464e1
Jack Gifford <jack@jackgifford.me>
2019-12-02 19:44:13 +1100

    Working with demo

--- days/day02.go
+++ days/day02.go
@@ -4,6 +4,62 @@ import (
 	"fmt"
 )
 
+var machineState [5]int = [5]int{1, 0, 0, 0, 99}
+var i int = 0
+
+func systemState() {
+	fmt.Println(machineState)
+}
+
+func addOp() {
+	inputOne := machineState[machineState[i+1]]
+	inputTwo := machineState[machineState[i+2]]
+	resReg := machineState[i+3]
+
+	machineState[resReg] = inputOne + inputTwo
+}
+
+func multOp() {
+	inputOne := machineState[machineState[i+1]]
+	inputTwo := machineState[machineState[i+2]]
+	resReg := machineState[i+3]
+
+	machineState[resReg] = inputOne * inputTwo
+}
+
+func haltOp() {
+	fmt.Println("Program finished :)")
+	systemState()
+}
+
+func loop() {
+	i = 0	
+	for {
+		opCode := machineState[i]
+
+		if opCode == 1 {
+			addOp()
+		} else if opCode == 2 {
+			multOp()
+		} else if opCode == 99 {
+			haltOp()
+			break
+		} else {
+			panic("Machine broken!")
+		}
+
+		i += 4
+		systemState()
+	}
+}
+
 func Run() {
-	fmt.Printf("I am a stub\n")
+	fmt.Println("Test One")
+	loop()
+
+	fmt.Println("Test two")
+	machineState = [5]int{2,3,0,3,99}
+	resetSystem()
+	loop()
+
 }
