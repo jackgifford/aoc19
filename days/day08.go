e5af2a78b2b1201588509f5d7dfafca3c7b01a8f
Jack Gifford <jack@jackgifford.me>
2019-12-08 18:31:57 +1100

    finished day08 part 2

--- days/day08.go
+++ days/day08.go
@@ -15,13 +15,14 @@ func getZeros(layer string, val rune) int {
 	return count
 }
 
-func main() {
-	length := 25
-	height := 6
+func partOne() {
+	length := 3
+	height := 2
 	pixels := length * height
 
 	start := 0
 	end := pixels
+	data := "123456789012"
 
 	var maxLayer string
 	maxCount := 1 <<31 -1
@@ -42,4 +43,46 @@ func main() {
 
 	fmt.Printf("Least zeroes %v\n", maxLayer)
 	fmt.Printf("%v\n", getZeros(maxLayer, '1') * getZeros(maxLayer, '2'))
+}
+
+func partTwo() {
+	length := 25
+	height := 6
+
+	pixels := length * height
+	data = "0000"
+
+	// need immutability
+	baseLayer := []byte(data[0:pixels])
+
+	start := pixels
+	end := pixels * 2
+
+	for end <= len(data) {
+		layer := data[start:end]
+		start += pixels
+		end += pixels
+		
+		for i, v := range layer {
+			if rune(baseLayer[i]) == '2' {
+				baseLayer[i] = byte(v)
+			}
+		}
+	}
+	
+	final := string(baseLayer)
+	start = 0
+	end = length
+
+	for end <= len(final) {
+			fmt.Println(final[start:end])
+			start += length
+			end += length
+	}
+
+}
+
+func main() {
+	//partOne()
+	partTwo()
 }
