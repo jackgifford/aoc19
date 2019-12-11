bf2c70123d92e736845f032fcc900b44fb416a6b
Jack Gifford <jack@jackgifford.me>
2019-12-11 21:33:46 +1100

    day09 submission

--- intcode/complex.go
+++ intcode/complex.go
@@ -6,19 +6,15 @@ import (
 
 func (state *IntcodeState) systemState() {
 	fmt.Printf("Final state: ")
-	fmt.Println(state.machineState)
+	//fmt.Println(state.machineState)
 }
 
-func (state *IntcodeState) addOp(inputOne int, inputTwo int) {
-	resReg := state.machineState[state.i+3]
-
-	state.machineState[resReg] = inputOne + inputTwo
+func (state *IntcodeState) addOp(inputOne int, inputTwo int, resReg int) {
+	state.machineState[resReg] = state.machineState[inputOne] + state.machineState[inputTwo]
 }
 
-func (state *IntcodeState) multOp(inputOne int, inputTwo int) {
-	resReg := state.machineState[state.i+3]
-
-	state.machineState[resReg] = inputOne * inputTwo
+func (state *IntcodeState) multOp(inputOne int, inputTwo int, resReg int) {
+	state.machineState[resReg] = state.machineState[inputOne] * state.machineState[inputTwo]
 }
 
 func (state *IntcodeState) inputOp(location int) {
@@ -28,37 +24,37 @@ func (state *IntcodeState) inputOp(location int) {
 }
 
 func (state *IntcodeState) outputOp(input int) {
-	state.output(input, state.GetName())
+	state.output(state.machineState[input], state.GetName())
 }
 
 func (state *IntcodeState) jumpIfTrueOp(inputOne int, inputTwo int) bool {
-	if inputOne != 0 {
-		state.i = inputTwo
+	if state.machineState[inputOne] != 0 {
+		state.i = state.machineState[inputTwo]
 	}
 
-	return inputOne != 0
+	return state.machineState[inputOne] != 0
 }
 
 func (state *IntcodeState) jumpIfFalseOp(inputOne int, inputTwo int) bool {
-	if inputOne == 0 {
-		state.i = inputTwo
+	if state.machineState[inputOne] == 0 {
+		state.i = state.machineState[inputTwo]
 	}
 
-	return inputOne == 0
+	return state.machineState[inputOne] == 0
 }
 
-func (state *IntcodeState) lessThanOp(inputOne int, inputTwo int) {
-	resReg := state.machineState[state.i+3]
-	if inputOne < inputTwo {
+func (state *IntcodeState) lessThanOp(inputOne int, inputTwo int, resReg int) {
+	//resReg := state.machineState[resReg]
+	if state.machineState[inputOne] < state.machineState[inputTwo] {
 		state.machineState[resReg] = 1
 	} else {
 		state.machineState[resReg] = 0
 	}
 }
 
-func (state *IntcodeState) equalsOp(inputOne int, inputTwo int) {
-	resReg := state.machineState[state.i+3]
-	if inputOne == inputTwo {
+func (state *IntcodeState) equalsOp(inputOne int, inputTwo int, resReg int) {
+	//resReg := state.machineState[resReg]
+	if state.machineState[inputOne] == state.machineState[inputTwo] {
 		state.machineState[resReg] = 1
 	} else {
 		state.machineState[resReg] = 0
@@ -79,8 +75,8 @@ func (state *IntcodeState) validate(position int) (int, [3]int) {
 	var argCount int
 
 	if opcode == 1 || opcode == 2 || opcode == 5 || opcode == 6 || opcode == 7 || opcode == 8 {
-		argCount = 2
-	} else if opcode == 3 || opcode == 4 {
+		argCount = 3
+	} else if opcode == 3 || opcode == 4 || opcode == 9 {
 		argCount = 1
 	} else {
 		argCount = 0
@@ -90,9 +86,11 @@ func (state *IntcodeState) validate(position int) (int, [3]int) {
 		modeType := instruction % 10
 		instruction /= 10
 		if modeType == 1 {
-			arr[curr] = state.machineState[curr+position+1]
+			arr[curr] = curr+position+1
+		} else if modeType == 2 {
+			arr[curr] = state.machineState[curr+position+1] +  state.base	
 		} else {
-			arr[curr] = state.machineState[state.machineState[curr+position+1]]
+			arr[curr] = state.machineState[curr+position+1]
 		}
 	}
 
@@ -105,13 +103,13 @@ func (state *IntcodeState) Loop() {
 		opCode, inputs := state.validate(state.i)
 
 		if opCode == 1 {
-			state.addOp(inputs[0], inputs[1])
+			state.addOp(inputs[0], inputs[1], inputs[2])
 			state.i += 4
 		} else if opCode == 2 {
-			state.multOp(inputs[0], inputs[1])
+			state.multOp(inputs[0], inputs[1], inputs[2])
 			state.i += 4
 		} else if opCode == 3 {
-			state.inputOp(state.machineState[state.i+1])
+			state.inputOp(inputs[0])
 			state.i += 2
 		} else if opCode == 4 {
 			state.outputOp(inputs[0])
@@ -127,11 +125,14 @@ func (state *IntcodeState) Loop() {
 				state.i += 3
 			}
 		} else if opCode == 7 {
-			state.lessThanOp(inputs[0], inputs[1])
+			state.lessThanOp(inputs[0], inputs[1], inputs[2])
 			state.i += 4
 		} else if opCode == 8 {
-			state.equalsOp(inputs[0], inputs[1])
+			state.equalsOp(inputs[0], inputs[1], inputs[2])
 			state.i += 4
+		} else if opCode == 9 {
+			state.base += state.machineState[inputs[0]]
+			state.i += 2
 		} else if opCode == 99 {
 			state.haltOp()
 			break
@@ -152,6 +153,7 @@ type IntcodeState struct {
 	output func(int, int) 
 	input func(int) int
 	name int
+	base int
 }
 
 func NewProgram(initialState []int, name int, output func(int, int), input func(int) int) *IntcodeState {
@@ -162,6 +164,7 @@ func NewProgram(initialState []int, name int, output func(int, int), input func(
 	intcodeState.machineState = make([]int, len(initialState))
 	intcodeState.name = name
 	copy(intcodeState.machineState, initialState)
+	intcodeState.base = 0
 
 	return intcodeState
 }
